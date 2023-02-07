4572f381a848b369ed7cd3489f35fea3df54b918
Inspiredolajosh <feranmiolawore@gmail.com>
2023-02-07 10:41:58 +0100

    bug fix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 async function main() {
   const taxFee = 5
-  const Contract = await hre.ethers.getContractFactory('Genesis')
+  const Contract = await hre.ethers.getContractFactory('Schoolcrowdfund')
   const contract = await Contract.deploy(taxFee)
 
   await contract.deployed()
